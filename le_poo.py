41de01d0ffaf84b2f00a674e369877d0313f5655
GleciaGaba <gleciasf@hotmail.com>
2024-07-31 17:47:43 +0200

    first commit

---
+++ le_poo.py
@@ -0,0 +1,13 @@
+class Voiture:
+    marque = "Lamborghini"
+    couleur = "rouge"
+
+
+voiture_01 = Voiture()
+voiture_02 = Voiture()
+
+print(voiture_01.marque)
+print(voiture_01.couleur)
+print(voiture_02.marque)
+print(voiture_02.couleur)
+
